d4b1d4aad13c6c94af57c29550f26e0adb95f983
Garabed Baghsarian <garabed.baghsarian@gmail.com>
2022-05-07 01:11:57 +0400

    updated the tests

--- cypress/integration/3-software-testing-HW5/searchResult.spec.js
+++ cypress/integration/3-software-testing-HW5/searchResult.spec.js
@@ -2,12 +2,16 @@ describe("testing MDN's search functionality with Cypress", () => {
   beforeEach(() => {
     cy.visit("https://developer.mozilla.org/en-US/");
   });
-  it("inputs keywords into search input", () => {
+  it("inputs keyword into search input and checks title", () => {
     cy.get("#top-nav-search-q").click().type(".reduce(){enter}");
-    cy.get("#languages-switcher-button > .button-wrap").click();
-    cy.get(".submenu > :nth-child(3) > .button > span").click();
-    cy.get(".main-page-content > :nth-child(2) > p").should("contain",
-      "La méthode reduce() applique une fonction"
-    );
+    cy.get("h1").should("have.text", "Array.prototype.reduce()");
+  });
+
+  it("checks career in marketing in US", () => {
+    cy.get(".page-footer-nav-col-1 > .footer-nav-list > :nth-child(3) > a")
+      .invoke("removeAttr", "target")
+      .click();
+    cy.url().should("include", "/careers/listings");
+    cy.get("main > :nth-child(3)").should("contain.text", "Remote US");
   });
 });
